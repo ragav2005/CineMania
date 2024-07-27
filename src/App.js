628bcef37c6d9c12214626f5138452eeb334a07c
ragav vignes <74288437+ragav2005@users.noreply.github.com>
2024-07-27 22:06:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -249,7 +249,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
       document.title = `Movie | ${title}`;
 
       return function () {
-        document.title = "usePopcorn";
+        document.title = "CineMania";
       };
     },
     [title]
